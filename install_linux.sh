2dd200ce7a91fe25198bacfc3868d9d866d647b2
Abhishek Ranjan <abhishekr700@gmail.com>
2020-03-25 22:23:37 +0530

    Add requirements.txt & install script
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "=== Repo Manager Install ==="
-echo "Making repomgy.py executable"
+echo "=== Repo Manager Install ===" 
+echo "=> Installing dependencies"
+pip3 install -r requirements.txt
+echo "=> Making repomgy.py executable"
 chmod +x repomgr.py
-echo "Copying files to /usr/bin"
+echo "=> Copying files to /usr/bin"
 cp repomgr.py /usr/bin/repomgr
 cp commandLiveOutput.py /usr/bin/
-echo "Done ! Reload your shell"
+echo "=> Done ! Reload your shell"
