7692d9946f3d10ae7c6c26d273998a09a5145c03
Abhishek Ranjan <abhishekr700@gmail.com>
2020-03-26 00:11:45 +0530

    WIP
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- commandLiveOutput.py
+++ commandLiveOutput.py
@@ -16,7 +16,7 @@ import shlex
 def execCommandLive(cmd, dir = None):
     print("Exec Command: ",cmd,"Directory: ",dir)
     if dir is None:
-        process = subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE)
+        process = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
     else:
         process = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, cwd=dir)
 
